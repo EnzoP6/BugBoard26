2a93e7cb44e1bad16674bdf5a33cad9c1cd41fd9
EnzoP6 <106398215+EnzoP6@users.noreply.github.com>
2026-06-11 18:00:56 +0200

    Add files via upload

---
+++ backend/src/main/java/com/bugboard26/issue/enums/IssueStatus.java
@@ -0,0 +1,8 @@
+package com.bugboard26.issue.enums;
+
+public enum IssueStatus {
+    TODO,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
---
+++ backend/src/main/java/com/bugboard26/issue/enums/IssueType.java
@@ -0,0 +1,8 @@
+package com.bugboard26.issue.enums;
+
+public enum IssueType {
+    QUESTION,
+    BUG,
+    DOCUMENTATION,
+    FEATURE
+}
